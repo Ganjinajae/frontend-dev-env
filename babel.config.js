dd4399fff8fc21e3df7425482626d474b7f3e3ff
지영진 <wldudwls12@gmail.com>
2022-07-20 23:04:20 +0900

    feat: Promise 객체 ES5로 변환할 수 있도록 추가

--- babel.config.js
+++ babel.config.js
@@ -6,10 +6,14 @@ module.exports = {
   // ]
   presets: [
     // './my-babel-preset.js'
-    ['@babel/preset-env', {
+    ['@babel/preset-env', { // 변환할 수 있는 것만 변환할 수 있다. 
       targets: {
         chrome: '79',
         ie: '11'
+      },
+      useBuiltIns: 'usage', // 'entry', false
+      corejs: {
+        version: 2,
       }
     }]
   ]
