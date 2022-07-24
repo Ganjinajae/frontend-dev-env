b4ac513c84e9ca55ad112aefbd3780c1df9d7ace
지영진 <wldudwls12@gmail.com>
2022-07-24 21:15:41 +0900

    feat: eslint-plugin-prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": ["eslint:recommended", 'eslint-config-prettier'], // eslint와 겹치는 설정이 있으면 꺼버리는 extension
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"], // eslint와 겹치는 설정이 있으면 꺼버리는 extension
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
