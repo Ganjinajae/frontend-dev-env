e071c5db1bb3b4598e4c7904ec01192eea832c21
지영진 <wldudwls12@gmail.com>
2022-07-26 15:28:18 +0900

    feat: axios와 같이 실습

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-// var foo = "";
+import axios from "axios";
 
-console.log();
+document.addEventListener("DOMContentLoaded", async () => {
+  const res = await axios.get("/api/users");
+  console.log(res);
+
+  document.body.innerHTML = (res.data || [])
+    .map((user) => {
+      return `<div>${user.id}: ${user.name}</div>`;
+    })
+    .join("");
+});
