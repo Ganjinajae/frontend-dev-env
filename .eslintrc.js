12b388eaa4f584aaf27bafdeabc7cab3a9103d33
지영진 <wldudwls12@gmail.com>
2022-07-26 15:12:36 +0900

    feat: eslint node 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ["eslint:recommended", "eslint-config-prettier"], // eslint와 겹치는 설정이 있으면 꺼버리는 extension
   parserOptions: {
